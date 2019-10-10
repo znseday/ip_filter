a0e06490c8a08b17952dd3c593ee9fc7e1c3aacd
znseday <55926888+znseday@users.noreply.github.com>
2019-10-11 01:01:12 +0300

    Update my_test.cpp

--- my_test.cpp
+++ my_test.cpp
@@ -7,29 +7,27 @@
 using namespace std;
 
 BOOST_AUTO_TEST_SUITE(ip_filter_test_suite)
-/*
+
 BOOST_AUTO_TEST_CASE(ip_filter_test_MyVersion)
 {
     BOOST_CHECK(MyVersion() > 0);
 }
-*/
+
 BOOST_AUTO_TEST_CASE(ip_filter_test_ConvertLineToIP)
 {
-	cout << "This is My Test !!!!!!!!!!!!!!" << endl;
 	string TestString = "100.2.30.44";
 	ipType ip = ConvertLineToIP(TestString);
-	cout << (int)ip.Bytes[0]<< endl;
 	BOOST_CHECK(ip.Bytes[0] == 100 && ip.Bytes[1] == 2 && ip.Bytes[2] == 30 && ip.Bytes[3] == 44);
 }
-/*
+
 BOOST_AUTO_TEST_CASE(ip_filter_test_FilterByBytes)
 {
 	string TestString = "100.2.30.44";
 	
 	ipType ip = ConvertLineToIP(TestString);
-	//cout << "ip for test: " << ip;
-	//cout << "result ip (must be the same): ";
-	//FilterByBytes(cout, ip, (unsigned char)100, (unsigned char)2);
+	cout << "ip for test: " << ip;
+	cout << "result ip (must be the same): ";
+	FilterByBytes(cout, ip, (unsigned char)100, (unsigned char)2);
 	stringstream ss;
 	FilterByBytes(ss, ip, (unsigned char)100, (unsigned char)2);
 	string test;
@@ -37,6 +35,6 @@ BOOST_AUTO_TEST_CASE(ip_filter_test_FilterByBytes)
 
 	BOOST_CHECK(TestString == test);
 }
-*/
+
 
 BOOST_AUTO_TEST_SUITE_END()
