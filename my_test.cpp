9ad4e52ecb4cead647623ed25af9eb9e2026f7ed
znseday <55926888+znseday@users.noreply.github.com>
2019-10-11 00:50:36 +0300

    Update my_test.cpp

--- my_test.cpp
+++ my_test.cpp
@@ -12,14 +12,14 @@ BOOST_AUTO_TEST_CASE(ip_filter_test_MyVersion)
 {
     BOOST_CHECK(MyVersion() > 0);
 }
-/*
+
 BOOST_AUTO_TEST_CASE(ip_filter_test_ConvertLineToIP)
 {
 	string TestString = "100.2.30.44";
 	ipType ip = ConvertLineToIP(TestString);
 	BOOST_CHECK(ip.Bytes[0] == 100 && ip.Bytes[1] == 2 && ip.Bytes[2] == 30 && ip.Bytes[3] == 44);
 }
-
+/*
 BOOST_AUTO_TEST_CASE(ip_filter_test_FilterByBytes)
 {
 	string TestString = "100.2.30.44";
